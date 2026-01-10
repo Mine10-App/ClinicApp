84f90cad99f073ddd5fb47c942e179d4c58d1195
Mine <air1345@gmail.com>
2026-01-10 16:56:14 +0500

    Update firebase-service.js

--- firebase-service.js
+++ firebase-service.js
@@ -1,4 +1,7 @@
-const firebaseConfig = {
+// firebase-service.js - Secondary (named) app
+import { initializeApp } from "firebase/app";
+
+const firebaseServiceConfig = {
   apiKey: "AIzaSyAdWuyx6tI0bu5FSJelSBTrCPzAEOqM8Sc",
   authDomain: "clinicappser.firebaseapp.com",
   projectId: "clinicappser",
@@ -7,3 +10,8 @@ const firebaseConfig = {
   appId: "1:1003991615080:web:8ec314b79ff20a86a89ddd",
   measurementId: "G-MHKJV1L1T6"
 };
+
+
+const serviceApp = initializeApp(firebaseServiceConfig, "serviceApp"); // Named app
+
+export { serviceApp };
