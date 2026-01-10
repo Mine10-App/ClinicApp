7b6e831967ed82ee971666b60854852f9a5ce97a
Mine <air1345@gmail.com>
2026-01-10 16:53:32 +0500

    Create firebase-service.js

---
+++ firebase-service.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAdWuyx6tI0bu5FSJelSBTrCPzAEOqM8Sc",
+  authDomain: "clinicappser.firebaseapp.com",
+  projectId: "clinicappser",
+  storageBucket: "clinicappser.firebasestorage.app",
+  messagingSenderId: "1003991615080",
+  appId: "1:1003991615080:web:8ec314b79ff20a86a89ddd",
+  measurementId: "G-MHKJV1L1T6"
+};
